71c060e6b370a5d329fd7d705995fdf27a4feefa
ZukTol <sldancer@yandex.ru>
2025-06-22 22:02:44 +0300

    update loader views after line-wrapping settings change

---
+++ src/utils/obsidian-utils.ts
@@ -0,0 +1,13 @@
+﻿import {App} from "obsidian";
+import BaseView from "../views/base-view";
+import {VIEW_TYPE_JSON, VIEW_TYPE_TXT} from "../constants";
+import JsonView from "../views/json-view";
+import TxtView from "../views/txt-view";
+
+export function getLoaderViews(app: App): BaseView[] {
+	const leaves = [
+		...app.workspace.getLeavesOfType(VIEW_TYPE_JSON).filter(l => l.view instanceof JsonView),
+		...app.workspace.getLeavesOfType(VIEW_TYPE_TXT).filter(l => l.view instanceof TxtView),
+	];
+	return leaves.map(l => l.view as BaseView);
+}
